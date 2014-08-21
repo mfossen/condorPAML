1a4d648b4af29dc5e2c8b4c14fd1ecde9a0593da
mfossen <mfossen@snpsnap.medmicro.wisc.edu>
2014-08-21 15:01:01 -0500

    Added basic command parsing

--- condorPAML.py
+++ condorPAML.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python2.6
 
-import os
-import glob
-import sys
-import subprocess
+import os,glob,sys,subprocess,getopt
 
 fastaDir = "./fastafiles"
 genewisepamlLocation = "/opt/PepPrograms/genewisepaml/genewisePAML.py"
 submitFileLocation = "/opt/PepPrograms/genewisepaml/submit.sub"
-# get options and list help info first
 
+def submit():
+    if not os.path.isdir(fastaDir): os.mkdir(fastaDir)
 
+    topdir = os.getcwd()+"/"+fastaDir
 
+    for root, dirs, files in os.walk('.'):
+        for fastafile in files: 
+            if root == fastaDir : break
+            if fastafile.endswith(".fasta"):
+                fullpath = os.path.realpath( root+"/%s" % str(fastafile) )
+                runpath = os.path.realpath( topdir + "/%s" % str(fastafile)[:-6] )
 
-# make directory to run from
+                if not os.path.isdir(runpath): os.mkdir(runpath)
 
-# make dirs within directory, using os.walk possibly
+                filename = runpath+"/%s" % str(fastafile) 
+                if os.path.lexists(filename):
+                    os.remove(filename)		
+                os.symlink(fullpath , filename)
+                
+                submitFile = runpath + "/submit.sub"
+                if os.path.lexists(submitFile): os.remove(submitFile)
+                os.symlink(submitFileLocation , submitFile )
 
+    os.chdir(topdir)
 
-# os.symlink codeml and submit.sub into the created directories, make sure they copy correctly when condor_submit is called, possibly symlink the .fasta files in as well?
+    for fastaDir in glob.glob(topdir+"/*"):
+        if os.path.isdir(fastaDir):
+            os.chdir(fastaDir)
+            print os.getcwd()
+            subprocess.Popen([genewisepamlLocation,"-a",glob.glob("*.fasta")[0] ])
 
-if not os.path.isdir(fastaDir): os.mkdir(fastaDir)
+def usage():
+    print """Usage: ./condorPAML.py 
+-h, --help\tprint out usage information
 
-topdir = os.getcwd()+"/"+fastaDir
+-s, --submit\tset up directories, make symlinks, and submit jobs to run on Condor
 
-for root, dirs, files in os.walk('.'):
-    for fastafile in files: 
-        if root == fastaDir : break
-        if fastafile.endswith(".fasta"):
-            fullpath = os.path.realpath( root+"/%s" % str(fastafile) )
-            runpath = os.path.realpath( topdir + "/%s" % str(fastafile)[:-6] )
+"""
+    
+def main(argv):
+    opts, args = getopt.getopt(argv, "hs", ["help","submit"] )
+    for opt, arg in opts:
+        if '-h' or "--help" in opt:
+            usage()
+        elif '-s' or "--submit" in opt:
+            submit()
 
-            if not os.path.isdir(runpath): os.mkdir(runpath)
+        else: usage()
+    print opts
+    print args
+    sys.exit(0)    
 
-            filename = runpath+"/%s" % str(fastafile) 
-            if os.path.lexists(filename):
-                os.remove(filename)		
-            os.symlink(fullpath , filename)
-            
-            submitFile = runpath + "/submit.sub"
-            if os.path.lexists(submitFile): os.remove(submitFile)
-            os.symlink(submitFileLocation , submitFile )
-
-os.chdir(topdir)
-
-for fastaDir in glob.glob(topdir+"/*"):
-    if os.path.isdir(fastaDir):
-        os.chdir(fastaDir)
-        print os.getcwd()
-        subprocess.Popen([genewisepamlLocation,"-a",glob.glob("*.fasta")[0] ])
-
-
-
-sys.exit(0)    
+main(sys.argv[1:])
