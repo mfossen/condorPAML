1d042e2f69c2ec1ba3485f2a8090624d389dfb0e
mfossen <mfossen@snpsnap.medmicro.wisc.edu>
2014-08-21 15:41:41 -0500

    Added more options

--- condorPAML.py
+++ condorPAML.py
@@ -5,8 +5,9 @@ import os,glob,sys,subprocess,getopt
 fastaDir = "./fastafiles"
 genewisepamlLocation = "/opt/PepPrograms/genewisepaml/genewisePAML.py"
 submitFileLocation = "/opt/PepPrograms/genewisepaml/submit.sub"
+debug = False
 
-def submit():
+def submit(fastaDir, genewisePAMLLocation, submitFileLocation, debug=False):
     if not os.path.isdir(fastaDir): os.mkdir(fastaDir)
 
     topdir = os.getcwd()+"/"+fastaDir
@@ -25,7 +26,7 @@ def submit():
                     os.remove(filename)		
                 os.symlink(fullpath , filename)
                 
-                submitFile = runpath + "/submit.sub"
+                submitFile = runpath + "/" + os.path.basename(submitFileLocation)
                 if os.path.lexists(submitFile): os.remove(submitFile)
                 os.symlink(submitFileLocation , submitFile )
 
@@ -34,7 +35,7 @@ def submit():
     for fastaDir in glob.glob(topdir+"/*"):
         if os.path.isdir(fastaDir):
             os.chdir(fastaDir)
-            print os.getcwd()
+            if debug == True: print os.getcwd()
             subprocess.Popen([genewisepamlLocation,"-a",glob.glob("*.fasta")[0] ])
 
 def usage():
@@ -43,19 +44,28 @@ def usage():
 
 -s, --submit\tset up directories, make symlinks, and submit jobs to run on Condor
 
+--debug\toutput more information to the terminal
 """
     
 def main(argv):
-    opts, args = getopt.getopt(argv, "hs", ["help","submit"] )
+    try:
+        opts, args = getopt.getopt(argv, "hs", ["help","submit","debug"] )
+    except getopt.GetoptError:
+        usage()
+        sys.exit(1)
+
     for opt, arg in opts:
-        if '-h' or "--help" in opt:
+        if "-h" or "--help" in opt:
             usage()
-        elif '-s' or "--submit" in opt:
-            submit()
+            sys.exit(0)
+
+        if "--debug" in opt: debug = True
+        
+        if "-s" or "--submit" in opt:
+            submit(fastaDir,genewisepamlLocation,submitFileLocation,debug)
 
         else: usage()
-    print opts
-    print args
+
     sys.exit(0)    
 
 main(sys.argv[1:])
